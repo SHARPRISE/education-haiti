06e3ee63e27912f5c137d5a80496bb9137df2a0f
Alexandre Erich Sébastien Georges <erichaitian97@gmail.com>
2016-04-07 05:30:33 -0500

    Blog Models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import SuccessStory
 
 # Register your models here.
+
+
+class SuccessStoryAdmin(admin.ModelAdmin):
+    list_display = ['title','description','author']
+    list_filter = ['created']
+    search_fields = ['title','description','content']
+    #date_hierarchy = 'created'
+    save_on_top = True
+    prepopulated_fields = {"slug":("title",)}
+
+admin.site.register(SuccessStory, SuccessStoryAdmin)
