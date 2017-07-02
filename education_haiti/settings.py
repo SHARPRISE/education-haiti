4fde6495c124ddc5faf23a395e16664230f7b105
Henri E. Francois <doublerico7@gmail.com>
2017-07-01 23:06:07 -0400

    changed location of media directory and modified success story image urls accordingly. Attempting to serve static files.

--- education_haiti/settings.py
+++ education_haiti/settings.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Internals
 # Auth
