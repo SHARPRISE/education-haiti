bf6f698e3c6b2481a4343133b46f0d4aec000442
Alexandre Georges <erichaitian97@gmail.com>
2017-07-15 08:43:34 -0400

    Fix Attempt #1
    Fix?

--- education_haiti/settings.py
+++ education_haiti/settings.py
@@ -147,7 +147,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(PROJECT_ROOT)
-MEDIA_URL = '/media/'
+MEDIA_URL = '/education_haiti/'
 
 
 # Extra places for collectstatic to find static files.
